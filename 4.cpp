b8de3b4ea964f6bc1f39903410d88d4f524b3ba0
WunCah <56233158+WunCah@users.noreply.github.com>
2019-10-14 12:01:03 +0700

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main ()
+
+ {
+
+ int umur;
+
+  int kode;
+       	cout << " Nur Wulan Cahyani \n";
+    	cout << " D4 Manajemen Informatika \n";
+    	cout << "=============================== \n\n";
+    	
+ cout<<"Program menentukan dapat memiliki KTP"<<endl;
+
+ cout<<"masukkan umur";
+
+ cin>>umur;
+
+ if(umur<17)
+
+ {
+
+ cout<<"anda belum waktunya memiliki KTP!";
+
+ }
+
+ else
+
+ {
+
+ cout<<"anda boleh memiliki KTP";
+
+ }
+
+ return 0;
+
+ }
---
+++ pembagian.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout<< "Nur Wulan Cahyani \n";
+	cout<< "D4 Manajemen Informatika \n";
+    int x,nilai,total,a;
+    cout<<"Buatlah Pembagian sampai 100 \n";
+    cout<<"mau sampai berapa pembagian?";cin>>nilai;
+    cout<<"masukkan bilangan yan mau dibagikan:";cin>>a;
+    cout<<"\n hasil pembagiannya :\n";
+    for(x=0;x<=nilai;x++)
+    {total=x/a;
+    cout<<x<<"/"<<a<<"="<<total<<endl;}
+    {
+     return 0;
+    }
+}
